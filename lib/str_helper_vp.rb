5c7871b4a37828ec9c4066d8968a3949f1fbf474
vincepay <varghapayandeh@gmail.com>
2018-10-15 10:49:36 -0600

    removing the .gem from the repo

--- lib/str_helper_vp.rb
+++ lib/str_helper_vp.rb
@@ -1,5 +1,30 @@
 require "str_helper_vp/version"
 
 module StrHelperVp
+
   # Your code goes here...
+  def self.reversify(str)
+    str.split('').reverse.join('')
+  end
+
+  def self.casify(str) 
+    # randomly choose uppercase or lowercase letters returned:
+    to_case = [:upcase, :downcase]
+    arr = str.split('')
+    arr.each_with_index do |letter, i|
+      this_case = to_case.sample
+      # if you have a symbol that represents a method, you can 
+      arr[i]= letter.send(this_case)
+    end
+    arr.join('')
+  end
+
+  def self.spacify(str, space=0)
+    # put that many spaces between each thing
+    spaces.times do
+      str = str.split('').join(' ') 
+    end
+    str
+  end
+
 end
